61644fd79b1bd9646345fb5d32267366d6c8931a
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2017-08-16 08:59:07 -0700

    mult implementation

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,39 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Load R0 into D
+// While R1 is > 0, R1 -> R1-- and R0 -> R0 + R0
+// If R1 eq 0, set R2 to R0 and go to final loop
+
+    @R2
+    M=0 // clear contents
+
+    @R0
+    D=M // D=R0
+    @END
+    D;JEQ // If R0 = 0, jump to result
+
+    @R1
+    D=M // D=R1
+    @END
+    D;JEQ // If R1 = 0, jump to result
+(LOOP)
+    @1
+    D=D-A // D=D-1
+    @R1
+    M=D  //R1 = R1--
+    @R0
+    D=M   // load R0 into D
+    @R2
+    M=D+M // R2 = R0 + R2
+
+    @R1
+    D=M // D=R1
+    @END
+    D;JEQ // If R1 has been decremented to 0, jump to result
+
+    @LOOP
+    0;JMP // Goto LOOP
+(END)
+    @END
+    0;JMP // Infinite loop
